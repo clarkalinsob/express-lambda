3d3b9273a8a6f84aa2ba88ca27315ceb03bbf877
Clark Egbert Alinsob <clark_alinsob@cody.inc>
2022-03-09 20:41:29 +0800

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'app',
+      script: 'server.js',
+      instances: 'max',
+      max_memory_restart: '256M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
